cd39cb3d7683f30a9ed78ee51d10fbd69a6ecd34
gregwhitaker <gwhitake@gmail.com>
2017-10-14 22:46:04 -0700

    Doing work

--- service/src/main/java/com/github/gregwhitaker/ratpackjwt/service/SecureEndpoint.java
+++ service/src/main/java/com/github/gregwhitaker/ratpackjwt/service/SecureEndpoint.java
@@ -1,8 +1,10 @@
 package com.github.gregwhitaker.ratpackjwt.service;
 
+import org.pac4j.core.profile.UserProfile;
 import org.pac4j.http.client.direct.ParameterClient;
 import ratpack.func.Action;
 import ratpack.handling.Chain;
+import ratpack.jackson.Jackson;
 import ratpack.pac4j.RatpackPac4j;
 
 import java.util.HashMap;
@@ -20,8 +22,17 @@ public class SecureEndpoint implements Action<Chain> {
         chain.prefix("secure", c -> {
             c.all(RatpackPac4j.requireAuth(ParameterClient.class));
             c.get(ctx -> {
-                Map<String, String> response = new HashMap<>();
+                UserProfile profile = ctx.get(UserProfile.class);
+
+                Map<String, Object> userInfo = new HashMap<>();
+                userInfo.put("email", profile.getAttribute("email"));
+                userInfo.put("displayName", profile.getAttribute("display_name"));
+                userInfo.put("roles", profile.getRoles());
+
+                Map<String, Object> response = new HashMap<>();
                 response.put("message", "This endpoint is protected by JWT");
+                response.put("user", userInfo);
+                
                 ctx.render(json(response));
             });
         });
